cb8a9b90bec5362dff66e09e5fe0a54a7276a33d
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-11-16 11:24:43 -0300

    CollisionActionSource extra condition

--- starships/src/main/scala/actionSource/collision/CollisionActionSource.scala
+++ starships/src/main/scala/actionSource/collision/CollisionActionSource.scala
@@ -9,7 +9,13 @@ case class CollisionActionSource(collisionsActionMap: Map[(EntityType, EntityTyp
   def createActions(gameData: GameData): List[Action] =
     val collisions = gameData.collisions
     collisions.flatMap(collision => {
-      val actionListTuple = collisionsActionMap(collision.getEntityTypeTuple)
+      val actionListTuple = collisionsActionMap.get(collision.getEntityTypeTuple) match {
+        case None => collisionsActionMap.get(collision.inverse.getEntityTypeTuple) match
+          case None => (List():List[CollisionActionBuilder], List(): List[CollisionActionBuilder])
+          case Some(value) => value
+        case Some(value) => value
+      }
+
       val collisionBuilderInput = CollisionActionBuilderInput(collision.rightEntity, collision.leftEntity)
       val firstEntityActions = actionListTuple._1.map(_.build(collisionBuilderInput))
       val secondEntityActions = actionListTuple._2.map(_.build(collisionBuilderInput.inverse))
--- starships/src/main/scala/gameData/Collision.scala
+++ starships/src/main/scala/gameData/Collision.scala
@@ -3,3 +3,4 @@ package gameData
 case class Collision(leftEntity: Entity, rightEntity: Entity):
   def getEntityTypeTuple:(EntityType, EntityType) =
     (leftEntity.entityType, rightEntity.entityType)
+  def inverse: Collision = this.copy(rightEntity, leftEntity)
