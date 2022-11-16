bf139669b2e30a012db41976e61690870be1370c
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-11-16 11:14:24 -0300

    CollisionActionSourceFactory

--- starships/src/main/scala/factory/actionSourceFactory/CollisionActionSourceFactory.scala
+++ starships/src/main/scala/factory/actionSourceFactory/CollisionActionSourceFactory.scala
@@ -1,14 +1,17 @@
-//package factory.actionSourceFactory
-//
-//import actionSource.ActionSource
-//import actionSource.collision.CollisionActionSource
-//import gameData.EntityType
-//
-//case class CollisionActionSourceFactory() extends ActionSourceFactory:
-//  def create(): ActionSource =
-//    CollisionActionSource(
-//      Map(
-//        (EntityType.SHIP, EntityType.ASTEROID) -> List()
-//      )
-//    )
-//
+package factory.actionSourceFactory
+
+import action.collision.damageAction.CollisionDamageActionBuilder
+import actionSource.ActionSource
+import actionSource.collision.CollisionActionSource
+import gameData.EntityType
+
+case class CollisionActionSourceFactory() extends ActionSourceFactory:
+  def create(): ActionSource =
+    CollisionActionSource(
+      Map(
+        (EntityType.SHIP, EntityType.ASTEROID) -> (List(CollisionDamageActionBuilder()), List(CollisionDamageActionBuilder())))
+        , (EntityType.SHIP, EntityType.SHOT) -> (List(CollisionDamageActionBuilder()), List(CollisionDamageActionBuilder())))
+        , (EntityType.ASTEROID, EntityType.SHOT) -> (List(CollisionDamageActionBuilder()), List(CollisionDamageActionBuilder())))
+
+    )
+
