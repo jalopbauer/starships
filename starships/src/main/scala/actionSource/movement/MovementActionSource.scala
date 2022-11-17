749b55677836b01e36b9c146980600f33fceb507
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-11-17 12:17:13 -0300

    MovementActionSource

--- starships/src/main/scala/actionSource/movement/MovementActionSource.scala
+++ starships/src/main/scala/actionSource/movement/MovementActionSource.scala
@@ -3,12 +3,19 @@ package actionSource.movement
 import action.Action
 import action.movement.{MovementAction, MovementActionBuilder, MovementActionBuilderInput}
 import actionSource.ActionSource
-import gameData.{EntityType, GameData}
+import gameData.{EntityType, GameData, KeyStroke}
 
-case class MovementActionSource(movementActionMap: Map[EntityType, List[MovementActionBuilder]]) extends ActionSource:
+case class MovementActionSource(movementActionMap: Map[EntityType, List[MovementActionBuilder]]
+                                , movementActionMapWithKeyStroke: Map[(Int, KeyStroke), List[MovementActionBuilder]]) extends ActionSource:
+//  Claramente esto muy feo y hay que cambiar algo
   def createActions(gameData: GameData): List[Action] =
     val entities = gameData.entities
     entities.flatMap(entity => {
+      val keyStrokeActionList = gameData.keyStrokes.flatMap(keystroke => {
+        val mapKey = (entity.id, keystroke)
+        movementActionMapWithKeyStroke.getOrElse(mapKey, List())
+      })
       val actionList = movementActionMap(entity.entityType)
       actionList.map(_.build(MovementActionBuilderInput(entity)))
+        ++ keyStrokeActionList.map(_.build(MovementActionBuilderInput(entity)))
     })
