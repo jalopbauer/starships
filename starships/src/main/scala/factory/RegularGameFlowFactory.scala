3f5dd3119b5dd16d6d8c993151c0b3a1707f7c08
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-11-17 14:03:32 -0300

    SpawnActionSourceFactory and RegularGameFlowFactory

--- starships/src/main/scala/factory/RegularGameFlowFactory.scala
+++ starships/src/main/scala/factory/RegularGameFlowFactory.scala
@@ -1,7 +1,13 @@
 package factory
 
+import factory.actionSourceFactory.{CollisionActionSourceFactory, MovementActionSourceFactory, SpawnActionSourceFactory}
 import gameFlow.GameFlow
 
 case class RegularGameFlowFactory():
+  val spawnActionSourceFactory: SpawnActionSourceFactory = SpawnActionSourceFactory()
+  val movementActionSourceFactory: MovementActionSourceFactory = MovementActionSourceFactory()
+  val collisionActionSourceFactory: CollisionActionSourceFactory = CollisionActionSourceFactory()
   def create(): GameFlow =
-    GameFlow(List())
+    GameFlow(List(spawnActionSourceFactory.create()
+      , movementActionSourceFactory.create()
+      , collisionActionSourceFactory.create()))
---
+++ starships/src/main/scala/factory/actionSourceFactory/SpawnActionSourceFactory.scala
@@ -0,0 +1,15 @@
+package factory.actionSourceFactory
+
+import action.movement.constantMovementAction.ConstantMovementActionBuilder
+import action.movement.constantRotationAction.ConstantRotationActionBuilder
+import action.movement.maxValueVariableMovementAction.MaxValueVariableMovementActionBuilder
+import action.movement.variableMovementAction.VariableMovementActionBuilder
+import actionSource.ActionSource
+import actionSource.movement.MovementActionSource
+import actionSource.spawn.SpawnActionSource
+import gameData.{EntityType, KeyStroke, KeyStrokeAction}
+
+case class SpawnActionSourceFactory() extends ActionSourceFactory:
+  def create(): ActionSource =
+    SpawnActionSource()
+
