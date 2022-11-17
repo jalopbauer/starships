e78a4c128994f5fd3cdbb8342bc1f0811ff54586
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-11-17 15:26:23 -0300

    DefaultGameData

---
+++ starships/src/main/scala/gameData/implementation/DefaultGameData.scala
@@ -0,0 +1,18 @@
+package gameData.implementation
+
+import action.Action
+import entity.Entity
+import entity.value.{Collision, KeyStroke}
+import gameData.GameData
+import spawner.Spawner
+
+case class DefaultGameData(collisions: List[Collision]
+                          , entitiesMap: Map[Int, Entity]
+                          , keyStrokes: List[KeyStroke]
+                          , spawners: List[Spawner]) extends GameData:
+
+  def receiveAction(action: Action): GameData =
+    val entity = action.act
+    this.copy(entitiesMap = entitiesMap + (entity.id -> entity))
+
+  def entities: List[Entity] = entitiesMap.values.toList
