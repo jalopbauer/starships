712f66e739c1ca578956fc43f80766e8b7c2dd11
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-11-24 17:11:55 -0300

    SpawnAsteroidAction

--- starships/src/main/scala/factory/actionSourceFactory/SpawnActionSourceFactory.scala
+++ starships/src/main/scala/factory/actionSourceFactory/SpawnActionSourceFactory.scala
@@ -1,15 +1,32 @@
 package factory.actionSourceFactory
 
-import action.movement.constantMovementAction.ConstantMovementActionBuilder
-import action.movement.constantRotationAction.ConstantRotationActionBuilder
-import action.movement.maxValueVariableMovementAction.MaxValueVariableMovementActionBuilder
-import action.movement.variableMovementAction.VariableMovementActionBuilder
+import action.spawn.*
 import actionSource.ActionSource
 import actionSource.movement.MovementActionSource
 import actionSource.spawn.SpawnActionSource
-import entity.value.{EntityType}
-
+import entity.value.motion.{Coordinate, Motion}
 case class SpawnActionSourceFactory() extends ActionSourceFactory:
   def create(): ActionSource =
-    SpawnActionSource()
+    SpawnActionSource(List(
+      ShotSpawnAction()
+      , SpawnAsteroidAction(Motion(Coordinate(1,2),Coordinate(0,0),0), 1, 5)
+      , SpawnAsteroidAction(Motion(Coordinate(2,1),Coordinate(0,0),0), 1, 5)
+      , SpawnAsteroidAction(Motion(Coordinate(3,2),Coordinate(0,0),0), 2, 2)
+      , SpawnAsteroidAction(Motion(Coordinate(2,3),Coordinate(0,0),0), 1, 2)
+
+      , SpawnAsteroidAction(Motion(Coordinate(-1, -2), Coordinate(800, 800), 0), 1, 5)
+      , SpawnAsteroidAction(Motion(Coordinate(-2, -1), Coordinate(800, 800), 0), 1, 5)
+      , SpawnAsteroidAction(Motion(Coordinate(-3, -2), Coordinate(800, 800), 0), 2, 2)
+      , SpawnAsteroidAction(Motion(Coordinate(-2, -3), Coordinate(800, 800), 0), 1, 2)
+
+      , SpawnAsteroidAction(Motion(Coordinate(1, -2), Coordinate(0, 800), 0), 1, 5)
+      , SpawnAsteroidAction(Motion(Coordinate(2, -1), Coordinate(0, 800), 0), 1, 5)
+      , SpawnAsteroidAction(Motion(Coordinate(3, -2), Coordinate(0, 800), 0), 2, 2)
+      , SpawnAsteroidAction(Motion(Coordinate(2, -3), Coordinate(0, 800), 0), 1, 2)
+
+      , SpawnAsteroidAction(Motion(Coordinate(-1, 2), Coordinate(800, 0), 0), 1, 5)
+      , SpawnAsteroidAction(Motion(Coordinate(-2, 1), Coordinate(800, 0), 0), 1, 5)
+      , SpawnAsteroidAction(Motion(Coordinate(-3, 2), Coordinate(800, 0), 0), 2, 2)
+      , SpawnAsteroidAction(Motion(Coordinate(-2, 3), Coordinate(800, 0), 0), 1, 2)
+    ))
 
