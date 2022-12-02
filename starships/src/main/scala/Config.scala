9e1a9ea7a49d373112528717371faeb1fb5c6ae3
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-02 11:28:08 -0300

    Config

--- starships/src/main/scala/Config.scala
+++ starships/src/main/scala/Config.scala
@@ -1,10 +1,9 @@
+
+import entity.value.motion.{Coordinate, Motion}
 import factory.actionSourceFactory.*
 import seed.*
 
 class Config:
-  
-//  Min value of 1
-  val amountOfPlayers: Int = 2
 
   val actionSources: List[ActionSourceFactory] = List(
     SpawnActionSourceFactory()
@@ -12,5 +11,13 @@ class Config:
     , CollisionActionSourceFactory()
   )
 
-  val intSeed: IntSeed = CrescendoIntSeed()
+//  Min value of 1
+  val amountOfPlayers: Int = 2
 
+  val intSeed: IntSeed = CrescendoIntSeed()
+  val booleanSeed: BooleanSeed = PatternBooleanSeed(List(true, true, false, true, false))
+
+  val shipMotionList: List[Motion] = List(Motion(Coordinate(0,0), Coordinate(200,200), 0)
+                                          , Motion(Coordinate(0,0), Coordinate(200,200), 0))
+  
+  
