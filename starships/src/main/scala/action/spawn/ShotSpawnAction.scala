49a1bf613999a0ddb951573d0637421f44ee3f7e
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-10 19:47:25 -0300

    fix shot spawn

--- starships/src/main/scala/action/spawn/ShotSpawnAction.scala
+++ starships/src/main/scala/action/spawn/ShotSpawnAction.scala
@@ -2,12 +2,13 @@ package action.spawn
 
 import action.spawn.SpawnAction
 import entity.Entity
-import entity.value.EntityType
+import entity.value.{EntityType, KeyPress}
 import gameState.PlayingGameState
 
 case class ShotSpawnAction() extends SpawnAction:
   def act(gameData: PlayingGameState): PlayingGameState =
     gameData.ships.foldLeft(gameData)((acc, ship) =>{
       val nextSeed = gameData.idSeed.next
-      acc.add(ship.copy(id = nextSeed._1, entityType = EntityType.SHOT, health = 1, damage = 1)).copy(idSeed = nextSeed._2)
+      if (gameData.keyPresses.contains(KeyPress("r")) && ship.id == 1) acc.add(ship.copy(id = nextSeed._1, entityType = EntityType.SHOT, health = 1, damage = 1)).copy(idSeed = nextSeed._2)
+      else acc
     })
