849f9ed057d44938d47615a66c1bee805b2f1f5f
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-11 13:53:41 -0300

    refactor TimePassedListener

--- starships/src/main/scala/jugar/integrationUi/listener/TimePassedListener.scala
+++ starships/src/main/scala/jugar/integrationUi/listener/TimePassedListener.scala
@@ -8,8 +8,6 @@ import jugar.integrationUi.adapter.{EntityAdapter, IdAdapter}
 case class TimePassedListener(mutableGameStateameData: MutableGameState
                               , gameFlow: GameFlow)
   extends EventListener[TimePassed] :
-  val idAdapter: IdAdapter = IdAdapter()
-  val entityAdapter: EntityAdapter = EntityAdapter()
 
   def handle(event: TimePassed): Unit =
     val gameData = mutableGameStateameData.gameData
