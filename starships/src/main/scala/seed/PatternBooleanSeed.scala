b66f46997e6074c002579adc19ab9b1cad362205
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-11 17:02:27 -0300

    removed print

--- starships/src/main/scala/seed/PatternBooleanSeed.scala
+++ starships/src/main/scala/seed/PatternBooleanSeed.scala
@@ -2,7 +2,6 @@ package seed
 
 case class PatternBooleanSeed(pattern: List[Boolean]) extends BooleanSeed:
   def next: (Boolean, PatternBooleanSeed) =
-    println(s"pattern = ${pattern}")
     val head = pattern.head
     (head, PatternBooleanSeed(pattern.tail :+ head))
 
