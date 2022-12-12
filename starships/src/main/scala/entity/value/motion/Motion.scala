849ad546fa98ab3915f66aba3e7f82680aa77254
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-12 20:58:08 -0300

    Motion rotation

--- starships/src/main/scala/entity/value/motion/Motion.scala
+++ starships/src/main/scala/entity/value/motion/Motion.scala
@@ -2,3 +2,7 @@ package entity.value.motion
 case class Motion(position: Coordinate, angleInDegrees: Double, speed: Speed):
   def moveForward(secondsSinceLastTime: Double): Motion =
     this.copy(speed = speed.moveForward(angleInDegrees, secondsSinceLastTime))
+  def rotateLeft(rotationAngleInDegrees: Double): Motion =
+    this.copy(angleInDegrees = angleInDegrees + rotationAngleInDegrees)
+  def rotateRight(rotationAngleInDegrees: Double): Motion =
+    this.copy(angleInDegrees = angleInDegrees - rotationAngleInDegrees)
