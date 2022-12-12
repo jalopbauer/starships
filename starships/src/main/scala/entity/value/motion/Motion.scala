a2335fc46c5e159af3dc1c9ca124ef40c3a06090
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-12 20:30:33 -0300

    Motion

--- starships/src/main/scala/entity/value/motion/Motion.scala
+++ starships/src/main/scala/entity/value/motion/Motion.scala
@@ -1,4 +1,4 @@
 package entity.value.motion
 case class Motion(position: Coordinate, angleInDegrees: Double, speed: Speed):
-  def move: Motion =
-    this.copy(speed = speed.move(angleInDegrees))
+  def move(secondsSinceLastTime: Double): Motion =
+    this.copy(speed = speed.move(angleInDegrees, secondsSinceLastTime))
