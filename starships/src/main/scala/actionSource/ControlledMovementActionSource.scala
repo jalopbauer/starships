6bd9433e6f7edfd0c1a03b3eeec32371dd69959f
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-13 19:52:40 -0300

    ControlledMovementActionSource

--- starships/src/main/scala/actionSource/ControlledMovementActionSource.scala
+++ starships/src/main/scala/actionSource/ControlledMovementActionSource.scala
@@ -1,9 +1,10 @@
 package actionSource
 
 import action.Action
+import action.movement.controlled.FullMotionControlledMovementAction
 import entity.value.ControlledMovementKeyPresses
 import gameState.PlayingGameState
 
 case class ControlledMovementActionSource(controlledMovementKeyPressesList: List[ControlledMovementKeyPresses]):
   def createActions(gameData: PlayingGameState): List[Action] =
-    List()
+    controlledMovementKeyPressesList.map(FullMotionControlledMovementAction(_))
