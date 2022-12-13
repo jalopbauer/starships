3f0e4cb3cbb0d3ceac1aba6e60baf34f6f0781bf
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-13 14:03:04 -0300

    SingleEntityAction

--- starships/src/main/scala/action/collision/DamageAction.scala
+++ starships/src/main/scala/action/collision/DamageAction.scala
@@ -1,10 +1,10 @@
 package action.collision
 
-import action.Action
+import action.{Action, SingleEntityAction}
 import entity.Entity
 import gameState.PlayingGameState
 
-case class DamageAction(entity: Entity, damage: Int) extends Action :
+case class DamageAction(entity: Entity, damage: Int) extends SingleEntityAction(entity) :
   def act(gameData: PlayingGameState): PlayingGameState =
     val health = entity.health - damage
     val damagedEntity = entity.copy(health = health)
