5a8d719934b5716cc695690533dd59abbb0491c5
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-13 12:40:52 -0300

    MovementActionSource

--- starships/src/main/scala/actionSource/movement/MovementActionSource.scala
+++ starships/src/main/scala/actionSource/movement/MovementActionSource.scala
@@ -1,16 +1,10 @@
-//package actionSource.movement
-//
-//import action.Action
-//import action.movement.MovementAction
-//import actionSource.ActionSource
-//import entity.value.{EntityType, KeyPress}
-//import gameState.PlayingGameState
-//
-//case class MovementActionSource(
-//                                 movementActionMap: Map[EntityType, MovementActionBuilder]
-//                                  ) extends ActionSource:
-//  def createActions(gameData: PlayingGameState): List[Action] =
-//    gameData.entities.map(entity =>
-//      movementActionMap(entity.entityType).build(MovementActionBuilderInput(entity))
-//    )
-//
+package actionSource.movement
+
+import action.movement.MovementAction
+import actionSource.ActionSource
+import gameState.PlayingGameState
+
+
+trait MovementActionSource extends ActionSource:
+  def createActions(gameData: PlayingGameState): List[MovementAction]
+
