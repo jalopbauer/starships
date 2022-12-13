9f5c18ffc88bf7eb8088b4316ae0a9cafd590cc7
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-13 15:31:50 -0300

    rename

--- starships/src/main/scala/action/movement/BackwardMovementAction.scala
+++ starships/src/main/scala/action/movement/BackwardMovementAction.scala
@@ -4,8 +4,8 @@ import action.SingleEntityAction
 import entity.Entity
 import gameState.PlayingGameState
 
-case class MoveForwardAction(entity: Entity) extends SingleEntityAction(entity):
+case class BackwardMovementAction(entity: Entity) extends SingleEntityAction(entity):
   def act(gameData: PlayingGameState): PlayingGameState =
-    val newMotion = entity.motion.moveForward(gameData.secondsSinceLastTime)
+    val newMotion = entity.motion.moveBackward(gameData.secondsSinceLastTime)
     val newEntity = entity.copy(motion = newMotion)
     gameData.add(newEntity)
--- starships/src/main/scala/action/movement/ForwardMovementAction.scala
+++ starships/src/main/scala/action/movement/ForwardMovementAction.scala
@@ -4,8 +4,8 @@ import action.SingleEntityAction
 import entity.Entity
 import gameState.PlayingGameState
 
-case class MoveBackwardAction(entity: Entity) extends SingleEntityAction(entity):
+case class ForwardMovementAction(entity: Entity) extends SingleEntityAction(entity):
   def act(gameData: PlayingGameState): PlayingGameState =
-    val newMotion = entity.motion.moveBackward(gameData.secondsSinceLastTime)
+    val newMotion = entity.motion.moveForward(gameData.secondsSinceLastTime)
     val newEntity = entity.copy(motion = newMotion)
     gameData.add(newEntity)
