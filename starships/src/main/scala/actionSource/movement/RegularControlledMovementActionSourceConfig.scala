eb84745cb7dfe5e91e7956d80615a46db654239d
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-13 23:52:25 -0300

    RegularControlledMovementActionSourceConfig

---
+++ starships/src/main/scala/actionSource/movement/RegularControlledMovementActionSourceConfig.scala
@@ -0,0 +1,25 @@
+package actionSource.movement
+
+import actionSource.ActionSourceConfig
+import entity.EntityIdControlledMovementKeyPresses
+import entity.value.{EntityType, KeyPress}
+
+case class RegularControlledMovementActionSourceConfig() extends ControlledMovementActionSourceConfig:
+  def controlledMovementKeyPressesList: List[EntityIdControlledMovementKeyPresses] =
+    List(
+      EntityIdControlledMovementKeyPresses(
+          1
+        , KeyPress("w")
+        , KeyPress("s")
+        , KeyPress("a")
+        , KeyPress("d")
+      ) ,
+      EntityIdControlledMovementKeyPresses(
+        2
+        , KeyPress("j")
+        , KeyPress("k")
+        , KeyPress("h")
+        , KeyPress("l")
+      )
+    )
+
