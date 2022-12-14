507b63347fe6b1499983eac9fda94d8b2867f125
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-13 23:30:00 -0300

    RegularTimedSpawnActionSourceConfig

---
+++ starships/src/main/scala/actionSource/spawn/RegularTimedSpawnActionSourceConfig.scala
@@ -0,0 +1,35 @@
+package actionSource.spawn
+
+import action.spawn.{SpawnAction, SpawnAsteroidAction}
+import actionSource.ActionSourceConfig
+import entity.value.motion.{Coordinate, Motion, Rotation, Speed}
+
+case class RegularTimedSpawnActionSourceConfig() extends TimedSpawnActionSourceConfig:
+  def time: Double = 10
+  def spawnActions: List[SpawnAction] =
+    val zeroZero = Coordinate(0,0)
+    val eightHundredZero = Coordinate(800,0)
+    val zeroEightHundred = Coordinate(0,800)
+    val eightHundredEightHundred = Coordinate(800,800)
+    val noRotation = Rotation(0, 0)
+    val speedOneOne = Speed(Coordinate(1, 1).normalize, 0, 0)
+    val speedThreeOne = Speed(Coordinate(3, 1).normalize, 0, 0)
+    val speedOneThree = Speed(Coordinate(1, 3).normalize, 0, 0)
+
+    List(
+      SpawnAsteroidAction(Motion(zeroZero, noRotation, speedOneOne), 1, 5)
+      , SpawnAsteroidAction(Motion(zeroZero, noRotation, speedThreeOne), 1, 5)
+      , SpawnAsteroidAction(Motion(zeroZero, noRotation, speedOneThree), 2, 2)
+
+      , SpawnAsteroidAction(Motion(eightHundredZero, noRotation, speedOneOne), 1, 5)
+      , SpawnAsteroidAction(Motion(eightHundredZero, noRotation, speedThreeOne), 1, 5)
+      , SpawnAsteroidAction(Motion(eightHundredZero, noRotation, speedOneThree), 2, 2)
+
+      , SpawnAsteroidAction(Motion(zeroEightHundred, noRotation, speedOneOne), 1, 5)
+      , SpawnAsteroidAction(Motion(zeroEightHundred, noRotation, speedThreeOne), 1, 5)
+      , SpawnAsteroidAction(Motion(zeroEightHundred, noRotation, speedOneThree), 2, 2)
+
+      , SpawnAsteroidAction(Motion(eightHundredEightHundred, noRotation, speedOneOne), 1, 5)
+      , SpawnAsteroidAction(Motion(eightHundredEightHundred, noRotation, speedThreeOne), 1, 5)
+      , SpawnAsteroidAction(Motion(eightHundredEightHundred, noRotation, speedOneThree), 2, 2)
+    )
