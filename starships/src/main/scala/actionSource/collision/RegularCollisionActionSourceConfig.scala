9b8ec07a92c64345353096f83c5c8bbcb3e5f0fe
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-13 23:06:31 -0300

    RegularCollisionActionSourceConfig

---
+++ starships/src/main/scala/actionSource/collision/RegularCollisionActionSourceConfig.scala
@@ -0,0 +1,13 @@
+package actionSource.collision
+
+import action.collision.{CollisionAction, CollisionDamageAction}
+import actionSource.ActionSourceConfig
+import entity.value.{Collision, EntityType, EntityTypeCollision}
+
+case class RegularCollisionActionSourceConfig() extends CollisionActionSourceConfig:
+  def collisionToMaybeCollisionAction(collision: Collision): Option[CollisionAction] =
+    if (List(EntityTypeCollision(EntityType.SHIP, EntityType.ASTEROID)
+      , EntityTypeCollision(EntityType.ASTEROID, EntityType.SHOT)
+      , EntityTypeCollision(EntityType.SHIP, EntityType.SHOT)
+    ).exists(EntityTypeCollision.compare(_, collision.entityTypeCollision))) Some(CollisionDamageAction(collision))
+    else None
