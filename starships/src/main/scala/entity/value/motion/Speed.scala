18edcf06ee9d7be55f43ff46084f9cc16b6dee81
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-14 15:45:28 -0300

    Speed

--- starships/src/main/scala/entity/value/motion/Speed.scala
+++ starships/src/main/scala/entity/value/motion/Speed.scala
@@ -2,11 +2,11 @@ package entity.value.motion
 
 case class Speed(speed: Coordinate, acceleration: Double, deacceleration: Double):
   def moveForward(angleInDegrees: Double, secondsSinceLastTime: Double): Speed =
-    val newSpeedAxisX = speed.x + Math.cos(Math.toRadians(angleInDegrees + 90)) * acceleration
-    val newSpeedAxisY = speed.y + Math.sin(Math.toRadians(angleInDegrees + 90)) * acceleration
-    this.copy(speed = speed.copy(newSpeedAxisX, newSpeedAxisY) * secondsSinceLastTime)
+    val newSpeedAxisX = speed.x + Math.cos(Math.toRadians(angleInDegrees)) * acceleration * secondsSinceLastTime
+    val newSpeedAxisY = speed.y + Math.sin(Math.toRadians(angleInDegrees)) * acceleration * secondsSinceLastTime
+    this.copy(speed = speed.copy(newSpeedAxisX, newSpeedAxisY))
 
   def moveBackward(angleInDegrees: Double, secondsSinceLastTime: Double): Speed =
-    val newSpeedAxisX = speed.x - Math.cos(Math.toRadians(angleInDegrees + 90)) / deacceleration
-    val newSpeedAxisY = speed.y - Math.sin(Math.toRadians(angleInDegrees + 90)) / deacceleration
-    this.copy(speed = speed.copy(newSpeedAxisX, newSpeedAxisY) * secondsSinceLastTime)
+    val newSpeedAxisX = speed.x - Math.cos(Math.toRadians(angleInDegrees)) / deacceleration / secondsSinceLastTime
+    val newSpeedAxisY = speed.y - Math.sin(Math.toRadians(angleInDegrees)) / deacceleration / secondsSinceLastTime
+    this.copy(speed = speed.copy(newSpeedAxisX, newSpeedAxisY))
