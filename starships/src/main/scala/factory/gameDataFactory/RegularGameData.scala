9c78278659a9ef11b092291ba6f73303cce99372
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-14 00:03:58 -0300

    RegularGameData

--- starships/src/main/scala/factory/gameDataFactory/RegularGameData.scala
+++ starships/src/main/scala/factory/gameDataFactory/RegularGameData.scala
@@ -1,7 +1,7 @@
 package factory.gameDataFactory
 
 import entity.Entity
-import entity.value.motion.{Coordinate, Motion}
+import entity.value.motion.{Coordinate, Motion, Rotation, Speed}
 import entity.value.EntityType
 import gameState.PlayingGameState
 import seed.{CrescendoIntSeed, PatternBooleanSeed}
@@ -10,10 +10,10 @@ case class RegularGameData() extends GameDataFactory:
   def create(): PlayingGameState =
     val seed = CrescendoIntSeed(3)
     val booleanSeed = PatternBooleanSeed(List(true, true, false, true, false))
-//    val motion = Motion(Coordinate(200,200), 0, 0)
+    val motion = Motion(Coordinate(300,200), Rotation(0 , 10), Speed(Coordinate(0,0), 0.3, 0.1))
     val entitiesMap: Map[Int, Entity] = Map(
-//      1 -> Entity(1, EntityType.SHIP, motion, 10, 5)
-//      , 2 -> Entity(2, EntityType.SHIP, motion, 10, 5)
+        1 -> Entity(1, EntityType.SHIP, motion, 10, 5)
+      , 2 -> Entity(2, EntityType.SHIP, motion, 10, 5)
     )
     PlayingGameState(entitiesMap, seed, booleanSeed)
 
