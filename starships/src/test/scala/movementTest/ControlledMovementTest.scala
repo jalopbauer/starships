7a9e1f046b6063a35f504ed48b3a0612ec7f43b9
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-14 18:31:41 -0300

    ControlledMovementTest

--- starships/src/test/scala/movementTest/ControlledMovementTest.scala
+++ starships/src/test/scala/movementTest/ControlledMovementTest.scala
@@ -14,15 +14,30 @@ class ControlledMovementTest extends AnyFunSuite {
   val booleanSeed: BooleanSeed = PatternBooleanSeed(List(true, true, false, true, false))
   val zeroZero: Coordinate = Coordinate(0, 0)
   val noRotation: Rotation = Rotation(0, 0)
-  val speedOneOne: Speed = Speed(Coordinate(1, 1), 0, 0)
-  val asteroid: Entity = Entity(1, EntityType.ASTEROID, Motion(zeroZero, noRotation, speedOneOne), 1, 5)
-  val gameData: PlayingGameState = PlayingGameState(Map(1 -> asteroid), seed, booleanSeed, secondsSinceLastTime = 1)
+
+  val speedWithAcceleration: Speed = Speed(zeroZero, 1, 0)
+  val shipWithAcceleration: Entity = Entity(1, EntityType.SHIP, Motion(zeroZero, noRotation, speedWithAcceleration), 1, 5)
+
+  val speedWithDeceleration: Speed = Speed(zeroZero, 0, 1)
+  val shipWithDeceleration: Entity = Entity(2, EntityType.SHIP, Motion(zeroZero, noRotation, speedWithDeceleration), 1, 5)
+
+  val speedWithBoth: Speed = Speed(zeroZero, 1, 1)
+  val shipWithBoth: Entity = Entity(3, EntityType.SHIP, Motion(zeroZero, noRotation, speedWithBoth), 1, 5)
+
+  val gameData: PlayingGameState = PlayingGameState(
+    Map(1 -> shipWithAcceleration, 2 -> shipWithDeceleration, 3 -> shipWithBoth)
+    , seed, booleanSeed, secondsSinceLastTime = 1)
+
   val gameFlow: GameFlow = GameFlow(List(ControlledMovementActionSource(RegularControlledMovementActionSourceConfig())))
 
-  test ("An empty Set should have size 0") {
+  test("No ship moves") {
     val playingGameState = gameFlow.nextFrame(gameData)
-    val entity = playingGameState.entity(1)
-    assert(entity.get.motion.position == Coordinate(1,1))
+    val entityOne = playingGameState.entity(1)
+    val entityTwo = playingGameState.entity(2)
+    val entityThree = playingGameState.entity(3)
+    assert(entityOne.get.motion.position == Coordinate(0, 0))
+    assert(entityTwo.get.motion.position == Coordinate(0, 0))
+    assert(entityThree.get.motion.position == Coordinate(0, 0))
   }
 
 }
