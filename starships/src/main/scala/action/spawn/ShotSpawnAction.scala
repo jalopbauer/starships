a03451bd2e6864a9bd040db7f5c7be11615223ed
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-14 21:41:43 -0300

    ShotSpawnAction

--- starships/src/main/scala/action/spawn/ShotSpawnAction.scala
+++ starships/src/main/scala/action/spawn/ShotSpawnAction.scala
@@ -2,6 +2,7 @@ package action.spawn
 
 import action.spawn.SpawnAction
 import entity.Entity
+import entity.value.motion.Coordinate
 import entity.value.{EntityType, KeyPress}
 import gameState.PlayingGameState
 
@@ -9,6 +10,14 @@ case class ShotSpawnAction() extends SpawnAction:
   def act(gameData: PlayingGameState): PlayingGameState =
     gameData.ships.foldLeft(gameData)((acc, ship) =>{
       val nextSeed = gameData.idSeed.next
-      if (gameData.keyPresses.contains(KeyPress("r")) && ship.id == 1) acc.add(ship.copy(id = nextSeed._1, entityType = EntityType.SHOT, health = 1, damage = 1)).copy(idSeed = nextSeed._2)
+      if (gameData.keyPresses.contains(KeyPress("r")) && ship.id == 1) {
+        val motion = ship.motion
+        val speed = motion.speed
+        val coordinate = speed.speed
+        val newSpeed = speed.copy(coordinate + Coordinate(coordinate.x + coordinate.x/coordinate.x, coordinate.y + coordinate.y/coordinate.y), 1, 1)
+        val newMotion = motion.copy(speed = newSpeed)
+        val entity = ship.copy(id = nextSeed._1, entityType = EntityType.SHOT, health = 1, damage = 1, motion = newMotion)
+        acc.add(entity).copy(idSeed = nextSeed._2)
+      }
       else acc
     })
