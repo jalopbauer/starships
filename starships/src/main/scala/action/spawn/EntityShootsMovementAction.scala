b0f35f565142a73f140e58324055c1bf42c14be0
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-15 23:24:29 -0300

    EntityShootsMovementAction

--- starships/src/main/scala/action/spawn/EntityShootsMovementAction.scala
+++ starships/src/main/scala/action/spawn/EntityShootsMovementAction.scala
@@ -13,9 +13,14 @@ case class EntityShootsMovementAction(id: Int) extends SpawnAction:
       val entity = maybeEntity.get
       val motion = entity.motion
       val speed = motion.speed
-      val coordinate = speed.speed
-      val newSpeed = speed.copy(coordinate + Coordinate(coordinate.x + coordinate.x / coordinate.x, coordinate.y + coordinate.y / coordinate.y), 1, 1)
-      val newMotion = motion.copy(speed = newSpeed)
+      val rotation = motion.rotation
+      val coordinate = motion.position
+      val speedCoordinate = speed.speed
+      val x = if (coordinate.x == 0) 1 else coordinate.x
+      val y = if (coordinate.y == 0) 1 else coordinate.y
+      val newSpeed = speed.copy(Coordinate((x / x.abs) * 10, (y / y.abs) * 10), 1, 1)
+      val newRotation = rotation.copy(angleInDegrees = rotation.angleInDegrees - 90)
+      val newMotion = motion.copy(speed = newSpeed, rotation = newRotation)
       val shot = entity.copy(id = nextSeed._1, entityType = EntityType.SHOT, health = 1, damage = 1, motion = newMotion)
       gameData.add(shot).copy(idSeed = nextSeed._2)
     else gameData
