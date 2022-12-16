3e1c262cba3c28871c2db054a4ee5ef56ab66575
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-12-15 21:57:57 -0300

    ConfigFile

--- starships/src/main/scala/config/ConfigFile.scala
+++ starships/src/main/scala/config/ConfigFile.scala
@@ -10,15 +10,15 @@ case object ConfigFile:
   val border: Int = 800
   val idSeed: IntSeed = CrescendoIntSeed()
   val booleanSeed: BooleanSeed = PatternBooleanSeed(List(true, true, false, true, false))
-  val shipShape: Shape = Shape(15, 10)
+  val shipShape: Shape = Shape(40, 30)
   val amountOfPlayers: Int = 2
   val playerShipsMotions: List[Motion] = List(
-      Motion(Coordinate(300,200), Rotation(0 , 10), Speed(Coordinate(0,0), 20, 20, 20))
-    , Motion(Coordinate(200,300), Rotation(0 , 10), Speed(Coordinate(0,0), 20, 20, 20))
+      Motion(Coordinate(300,200), Rotation(0 , 10), Speed(Coordinate(0,0), 10, 10, 20))
+    , Motion(Coordinate(200,300), Rotation(0 , 10), Speed(Coordinate(0,0), 10, 10, 20))
   )
 
   val playerShipsDamage: List[Int] = List(10, 10)
-  val playerShipsHealth: List[Int] = List(5, 5)
+  val playerShipsHealth: List[Int] = List(100, 100)
 
   val shipsControlledMovementKeyPresses: List[ControlledMovementKeyPresses] = List(
     ControlledMovementKeyPresses(
@@ -49,4 +49,4 @@ case object ConfigFile:
       Speed(Coordinate(1, 3).normalize, 0, 0, 20)
     )
   val asteroidHealth: List[Int] = List(1, 2, 3)
-  val asteroidDamage: List[Int] = List(3, 2, 1)
+  val asteroidDamage: List[Int] = List(1, 1, 1)
